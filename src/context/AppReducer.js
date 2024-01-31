e3a6c257dbb01f9d2f7645eefbb4b05763497227
0xsaad <saad2002hassan@gmail.com>
2024-01-31 11:31:47 -0800

    first commit

---
+++ src/context/AppReducer.js
@@ -0,0 +1,21 @@
+// Assign the arrow function to a variable before exporting
+const appReducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
+      };
+    default:
+      return state;
+  }
+};
+
+export default appReducer;
